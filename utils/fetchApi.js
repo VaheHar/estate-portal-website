2b4bb38c019b5669d3d14b45738f8892d84e1e8f
Vahe <91395022+VaheHar@users.noreply.github.com>
2022-03-12 23:26:31 +0400

    Update fetchApi.js

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
 
+//Keep your private keys safe. Not like this.
 export const fetchApi = async (url) => {
 	const { data } = await axios.get((url), {
 		headers: {
